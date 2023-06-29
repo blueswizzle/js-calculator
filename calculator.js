42f13633e56d1a9e78ad04e3302f18976c35cc39
Atheek Ashkar <atheek99@yahoo.com>
2023-06-29 14:51:21 -0400

    Added keyboard input functionality for numbers, negative toggle, and decmial toggle.
    Reworked the math operation fucntions and some other minor changes

--- calculator.js
+++ calculator.js
@@ -5,42 +5,71 @@ let firstNum = 0;
 let secondNum = 0;
 let operation = "";
 let result = 0;
-
+let prevOperation = "";
 screenTop.style.visibility = 'hidden';
+
+
+window.addEventListener('keydown', keyBoardInput);
 
 buttons.forEach(button =>{
     button.addEventListener('click', (e) =>{
         let input = e.target.id
         switch(input){
             case "AC":
-                screenMain.innerHTML = 0;
-                screenTop.innerHTML = 0;
+                screenMain.innerHTML = "0";
+                screenTop.innerHTML = "0";
                 screenTop.style.visibility = 'hidden';
+                operation = "";
+                prevOperation = "";
+                result = 0;
                 break;
             case "negative-toggle":
-                // do sometihng
+                negativeToggle();
                 break;
             case "divide":
-                divide();
+                if(operation !== ""){
+                    prevOperation = operation
+                    operation = "/";
+                }else{
+                    operation = "/"
+                }
+                performOperation()
                 break;
             case "multiply":
-                multipy();
+                if(operation !== ""){
+                    prevOperation = operation
+                    operation = "*";
+                }else{
+                    operation = "*"
+                }
+                performOperation()
                 break;
             case "addition":
-                add();
+                if(operation !== ""){
+                    prevOperation = operation
+                    operation = "+";
+                }else{
+                    operation = "+"
+                }
+                performOperation()
                 break;
             case "subtract":
-                subtract();
+                if(operation !== ""){
+                    prevOperation = operation
+                    operation = "-";
+                }else{
+                    operation = "-"
+                }
+                performOperation()
                 break;
             case "equals":
                 equals();
                 break;
             case "decimal":
-                //do something
+                decimal();
                 break;
             default:
-                let screenNumber = screenMain.innerHTML;
-                screenMain.innerHTML = parseInt(screenNumber + input).toString();
+                appendNumber(input);
                 break;
         }
         
@@ -49,100 +78,108 @@ buttons.forEach(button =>{
 
 
 function negativeToggle(){
-
-}
-
-function add(){
-    if(operation == "+"){
-        firstNum = parseInt(firstNum) + parseInt(screenMain.innerHTML)
-        screenTop.style.visibility = 'visible';
-        screenMain.innerHTML = 0;
-        screenTop.innerHTML = `${firstNum} ${operation} `;
-    }else{
-        firstNum = screenMain.innerHTML;
-        operation = "+"
-        screenMain.innerHTML = 0;
-        screenTop.style.visibility = 'visible';
-        screenTop.innerHTML = `${firstNum} ${operation} `;
-    }
-  
-}
-
-function subtract(){
-    if(operation == "-"){
-        firstNum = parseInt(firstNum) - parseInt(screenMain.innerHTML)
-        screenTop.style.visibility = 'visible';
-        screenMain.innerHTML = 0;
-        screenTop.innerHTML = `${firstNum} ${operation} `;
-    }else{
-        firstNum = screenMain.innerHTML;
-        operation = "-"
-        screenMain.innerHTML = 0;
-        screenTop.style.visibility = 'visible';
-        screenTop.innerHTML = `${firstNum} ${operation} `;
+    let num = screenMain.innerHTML
+    if(num != 0){
+        screenMain.innerHTML = num * -1;
     }
 }
 
-function multipy(){
-    if(operation == "*"){
-        firstNum = parseInt(firstNum) * parseInt(screenMain.innerHTML)
-        screenTop.style.visibility = 'visible';
-        screenMain.innerHTML = 0;
-        screenTop.innerHTML = `${firstNum} ${operation} `;
-    }else{
-        firstNum = screenMain.innerHTML;
-        operation = "*"
-        screenMain.innerHTML = 0;
-        screenTop.style.visibility = 'visible';
-        screenTop.innerHTML = `${firstNum} ${operation} `;
+function decimal(){
+    if(!screenMain.innerHTML.includes(".")){
+        screenMain.innerHTML += "."
     }
 }
 
-function divide(){
-    if(operation == "/"){
-        firstNum = parseInt(firstNum) / parseInt(screenMain.innerHTML)
-        screenTop.style.visibility = 'visible';
-        screenMain.innerHTML = 0;
-        screenTop.innerHTML = `${firstNum} ${operation} `;
+function performOperation(){
+    screenTop.style.visibility = 'visible'
+    if(prevOperation !== ""){
+        firstNum = getPreviousResult();
+        screenTop.innerHTML = `${firstNum} ${operation}`;
     }else{
-        firstNum = screenMain.innerHTML;
-        operation = "/"
-        screenMain.innerHTML = 0;
-        screenTop.style.visibility = 'visible';
-        screenTop.innerHTML = `${firstNum} ${operation} `;
-    }
+        firstNum = screenMain.innerHTML
+        screenTop.innerHTML = `${firstNum} ${operation}`;
+    }    
+    screenMain.innerHTML = "0";
+}
+
+function appendNumber(num){
+    screenMain.innerHTML = parseFloat(screenMain.innerHTML + num).toString();
+}
+function deleteNumber(){
+    screenMain.innerHTML = screenMain.innerHTML
+    .toString()
+    .slice(0, -1)
 }
 function equals(){
     secondNum = screenMain.innerHTML
     switch(operation){
         case "+":
-            result = parseInt(firstNum) + parseInt(secondNum)
+            result = parseFloat(firstNum) + parseFloat(secondNum)
             screenTop.innerHTML = `${firstNum} ${operation} ${secondNum} = `;
             screenMain.innerHTML = result;
             operation = ""
+            prevOperation = "";
+        
             break;
         case "-":
-            result = parseInt(firstNum) - parseInt(secondNum)
+            result = parseFloat(firstNum) - parseFloat(secondNum)
             screenTop.innerHTML = `${firstNum} ${operation} ${secondNum} = `;
             screenMain.innerHTML = result;
             operation = ""
+            prevOperation = "";
+            
             break;
         case "*":
-            result = parseInt(firstNum) * parseInt(secondNum)
+            result = parseFloat(firstNum) * parseFloat(secondNum)
             screenTop.innerHTML = `${firstNum} ${operation} ${secondNum} = `;
             screenMain.innerHTML = result;
             operation = ""
+            prevOperation = "";
+            
             break;
         case "/":
-            result = parseInt(firstNum) / parseInt(secondNum)
+            result = parseFloat(firstNum) / parseFloat(secondNum)
             result = roundNumber(result);
             screenTop.innerHTML = `${firstNum} ${operation} ${secondNum} = `;
             screenMain.innerHTML = result;
             operation = ""
+            prevOperation = "";
+            
             break; 
     }
 }
 
+function getPreviousResult(){
+    secondNum = screenMain.innerHTML
+    switch(prevOperation){
+        case "+":
+            result = parseFloat(firstNum) + parseFloat(secondNum)
+           
+            break;
+        case "-":
+            result = parseFloat(firstNum) - parseFloat(secondNum)
+            
+            break;
+        case "*":
+            result = parseFloat(firstNum) * parseFloat(secondNum)
+            
+            break;
+        case "/":
+            result = parseFloat(firstNum) / parseFloat(secondNum)
+            result = roundNumber(result);
+        
+            break; 
+    }
+    return result;
+}
+
 function roundNumber(number){
     return Math.round(number * 1000 + Number.EPSILON) / 1000;
+}
+
+
+function keyBoardInput(e){
+    if(e.key === 'Backspace') deleteNumber()
+    if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
+    if(e.key === '.') decimal()
 }
